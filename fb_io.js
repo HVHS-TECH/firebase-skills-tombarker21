413591218dab45995df44506e6ea142a13b17c5f
tombarker21 <23197tb@hvhs.school.nz>
2026-04-28 12:29:20 +1200

    28/04/26 pavel is very important to me

--- fb_io.js
+++ fb_io.js
@@ -6,3 +6,13 @@
  **                                                          **
  **************************************************************
  **************************************************************/
+ function fb_readError(error) {
+    console.log("There was an error reading the message");
+    console.error(error);
+  }
+
+
+  function fb_readlistener() {
+console.log(" is this Read Listener");
+firebase.database().ref('/message').on('value',  fb_logDatabaseRead)
+}
